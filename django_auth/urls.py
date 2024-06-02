9269db5af9df077c5acf9d9860b5cf9ac6dd7c9f
levab <lev.it.up0@gmail.com>
2024-06-03 01:04:58 +0500

    first commit

---
+++ django_auth/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('django.contrib.auth.urls')),
+    path('home/', include('users.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('users.urls')),
+]
